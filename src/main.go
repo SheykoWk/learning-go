0534ea018586a53c30d328655c3aa2995383bcd5
SheykoWk <sheyko.wk@gmail.com>
2022-01-03 16:05:16 -0600

    Delete hello world dir and create variables

---
+++ src/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Const declaration
+	const pi float64 = 3.1416
+	const pi2 = 3.141564
+	fmt.Println("pi:", pi)
+	fmt.Println("pi2:", pi2)
+	//Declaracion de variables enteras
+
+	base := 10
+
+	var altura int = 14
+	var area int = base * altura
+	fmt.Println("area:", area)
+
+	//ZERO values
+	var a int
+	var b float64
+	var c string
+	var d bool
+	fmt.Println(a, b, c, d)
+}
