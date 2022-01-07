1a9dbfd9be4cac1579a26f3a84f0e724c53ccc0c
SheykoWk <sheyko.wk@gmail.com>
2022-01-07 12:11:31 -0600

    Ciclo for

--- src/main.go
+++ src/main.go
@@ -2,29 +2,22 @@ package main
 
 import "fmt"
 
-func normalFunction(message string) {
-	fmt.Println(message)
-}
-func tripleArgument(a, b int, c string) {
-	fmt.Println(a, b, c)
-}
-
-func returnValue(a int) int {
-	return a * 2
-}
-
-func doubleReturn(a int) (c, d int) {
-	return a, a * 2
-}
-
 func main() {
-	normalFunction("Hola mundo")
-	tripleArgument(1, 2, "hola")
-	fmt.Println(returnValue(2))
-
-	value1, value2 := doubleReturn(2)
-	value3, _ := doubleReturn(8)
-
-	fmt.Println(value1, value2)
-	fmt.Println(value3)
+	//For conditional
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+	fmt.Printf("\n")
+	//For while
+	counter := 0
+	for counter < 10{
+		fmt.Println(counter)
+		counter++
+	}
+	//for forever
+	counterForever := 0
+	for {
+		fmt.Println(counterForever)
+		counterForever++
+	}
 }
