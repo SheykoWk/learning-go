388e964eac549ad341ec3563f81da8520635d3d2
SheykoWk <sheyko.wk@gmail.com>
2022-01-08 13:05:09 -0600

    Switch Case

--- src/main.go
+++ src/main.go
@@ -3,22 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	valor1 := 1
-	valor2 := 2
-	if valor1 == 1  {
-		fmt.Println("es 1")
-	} else {
-		fmt.Println("no es 1")
+	
+	switch module := 4 % 2; module{
+	case 0: 
+	fmt.Println("Es par")
+	default:
+		fmt.Println("Es impar")
 	}
-	// With and
-	if valor1 == 1 && valor2 == 2{
-		fmt.Println("Es verdad")
-	} 
-
-	//with or
-	if valor1 == 0 ||  valor2 == 2{
-		fmt.Println("Alguna de las 2 es correcta")
+	//switch without condition
+	value := 45
+	switch{
+	case value > 100:
+		fmt.Println("Es mayor a 100")
+	case value < 0:
+		fmt.Println("Es menor a 0")
+	default:
+		fmt.Println("No condicion")
 	}
-
 }
 
