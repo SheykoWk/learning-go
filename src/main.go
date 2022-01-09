0f3151a1d3ba021cc4e31bdcfd3f9d2068860297
SheykoWk <sheyko.wk@gmail.com>
2022-01-08 20:15:53 -0600

    Recorrido de Slices con Range

--- src/main.go
+++ src/main.go
@@ -2,29 +2,34 @@ package main
 
 import "fmt"
 
-func main() {
-	//array
-	var array [4]int
-	array[0] = 1
-	array[1] = 2
-	fmt.Println(array, len(array), cap(array))
-	//Slice
-	slice := []int{0,1,2,3,4,5,6}
-	fmt.Println(slice, len(slice), cap(slice))
+func idPalindromo(text string) {
+	var textReverse string
+	fmt.Scan(&textReverse)
 
-	//Metodos en slice
-	fmt.Println(slice[0])
-	fmt.Println(slice[:3])
-	fmt.Println(slice[2:4])
-	fmt.Println(slice[4:])
-
-	//append
-	slice = append(slice, 7)
-	fmt.Println(slice)
-
-	//append new list
-	newSlice := []int{8, 9, 10}
-	slice = append(slice, newSlice...)
-	fmt.Println(slice)
+	for i := len(text) - 1; i >= 0; i-- {
+		textReverse += string(text[i])
+	}
+	if text == textReverse {
+		fmt.Println("Es palindromo")
+	} else {
+		fmt.Println("No es palindromo")
+	}
+	fmt.Println(textReverse)
 }
 
+func main() {
+	slice := []string{"hola", "que", "hace"}
+	for i, valor := range slice {
+		//indice y valor
+		fmt.Println(i, valor)
+	}
+	for _, valor := range slice {
+		//solo valor
+		fmt.Println(valor)
+	}
+	for i := range slice {
+		//solo indice
+		fmt.Println(i)
+	}
+	idPalindromo("anita lava la tina")
+}
