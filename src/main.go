9ca52371495a02a4387a93b1177a65a4f3f48b37
SheykoWk <sheyko.wk@gmail.com>
2022-01-10 13:03:15 -0600

    Llave valor

--- src/main.go
+++ src/main.go
@@ -2,34 +2,22 @@ package main
 
 import "fmt"
 
-func idPalindromo(text string) {
-	var textReverse string
-	fmt.Scan(&textReverse)
-
-	for i := len(text) - 1; i >= 0; i-- {
-		textReverse += string(text[i])
-	}
-	if text == textReverse {
-		fmt.Println("Es palindromo")
-	} else {
-		fmt.Println("No es palindromo")
-	}
-	fmt.Println(textReverse)
-}
 
 func main() {
-	slice := []string{"hola", "que", "hace"}
-	for i, valor := range slice {
-		//indice y valor
-		fmt.Println(i, valor)
+	m := make(map[string]int)
+
+	m["Jose"] = 14
+	m["Pepito"] = 20
+	fmt.Println(m)
+
+	//Recorrer un map
+
+	for i, value:= range m{
+		fmt.Println(i, value)
 	}
-	for _, valor := range slice {
-		//solo valor
-		fmt.Println(valor)
-	}
-	for i := range slice {
-		//solo indice
-		fmt.Println(i)
-	}
-	idPalindromo("anita lava la tina")
+
+	//Encontrar un valor
+	value, ok := m["jose"]
+	fmt.Println(value, ok)
+
 }
